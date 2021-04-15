9a0626e064fe5b17eb8fc6fdf4f733394ee84e4f
WendelNunes <wendel.nunesdonizete@gmail.com>
2021-04-15 19:56:01 -0300

    Primeiro commit

---
+++ src/test/java/com/wendelnunes/multitenantschema/MultiTenantSchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendelnunes.multitenantschema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenantSchemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
